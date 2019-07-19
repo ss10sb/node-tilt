5207a1b77e2368371c3656ff7a7eae5c2bf75e05
Scott Morken <ss10sb@gmail.com>
2019-07-19 14:16:44 -0700

    remove sync

--- sync.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-USER=morken
-REMOTE=192.168.1.2
-REMOTE_PATH=/opt/node-tilt/
-echo "syncing to $REMOTE:$REMOTE_PATH"
-rsync -av -e ssh --exclude='node_modules' ./ ${USER}@${REMOTE}:${REMOTE_PATH}
